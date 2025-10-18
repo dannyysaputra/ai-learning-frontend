c69e5077d50f83b5e38dab8d5fb12ac0cf7fc9a9
dannyysaputra <84920467+dannyysaputra@users.noreply.github.com>
2025-10-18 18:53:07 +0700

    chore: Remove Netlify adapter for migration to Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
-
-import netlify from '@astrojs/netlify';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-
-  adapter: netlify()
 });
